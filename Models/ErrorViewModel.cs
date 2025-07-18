8909d4e2f5ed7f75b49009ff1b4e9376729c6d97
Guru Sai Krishna Thota <gurusaikrishna.thota@outlook.com>
2025-07-18 15:31:59 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hostel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
